30963f33299ebda1a66c99f3103d1c1275972d40
Nahom-hab <nahomhabtamu147@gmail.com>
2024-08-28 09:24:39 +0300

    added proxy iding functionality

--- services/node/route/bid.route.js
+++ services/node/route/bid.route.js
@@ -1,10 +1,11 @@
 
 import { Router } from 'express'
-import { placebid } from '../controller/bidding.controller.js'
+import { placebid, startProxyBid } from '../controller/bidding.controller.js'
 
 const router = Router()
 
 router.post('/', placebid)
+router.post('/proxybid/:id', startProxyBid)
 
 
 export default router
